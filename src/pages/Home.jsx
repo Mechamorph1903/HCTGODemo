34118c841d1925c942ce2be4b6b0d3ac76a2c305
Mechamorph1903 <danielanorue@gmail.com>
2026-06-09 12:29:48 -0500

    Live tracking of buses enabled, will implement interval for refresh soon. this pulls the data from the arcgis endpoint periodically and gets the geometry and name of bus icons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,18 +6,38 @@ import { useEffect, useState } from 'react'
 export default function Home() {
   //an array of objects contaning route data and their colors
     const routes = [
-    { stops: greenStops, color: '#38A800' }, //Green Route is ordered and complete
-    { stops: blueStops, color: '#0070FF' },
-    { stops: brownStops, color: '#732600' },
-    { stops: goldStops, color: '#E6E600' },
-    { stops: purpleStops, color: '#A900E6' },
-    { stops: orangeStops, color: '#FC921F' },
-    { stops: redStops, color: '#E60000' },
+    { stops: greenStops, color: '#38A800', name: 'Green'}, //Green Route is ordered and complete
+    { stops: blueStops, color: '#0070FF' , name: 'Blue'},
+    { stops: brownStops, color: '#732600' , name: 'Brown'},
+    { stops: goldStops, color: '#E6E600' , name: 'Gold'},
+    { stops: purpleStops, color: '#A900E6' , name: 'Purple'},
+    { stops: orangeStops, color: '#FC921F' , name: 'Orange'},
+    { stops: redStops, color: '#E60000' , name: 'Red'},
     
   ]
 
+  //This is where we will store and constantly update live bus positions
   const [busPositions, setBusPositions] = useState([])
+  console.log(busPositions)
 
+  useEffect(() => {
+
+    const fetchBusLive = async () => {
+      const response = await fetch("https://utility.arcgis.com/usrsvcs/servers/b02066689d504f5f9428029f7268e060/rest/services/Hosted/8bd5047cc5bf4195887cc5237cf0d3e0_Track_View/FeatureServer/1/query?f=json&where=1=1&outFields=*")
+      const data = await response.json()
+      setBusPositions(data.features)
+    }
+
+    fetchBusLive()
+  }, [])
+
+  const getBusColor = (createdUser) => {
+    const route = routes.find(route => createdUser.includes(route.name))
+    if(route){
+      return route.color
+    }
+    return "Gray"
+}
 
   return (
     <div className="flex flex-col items-center justify-center h-full text-black  text-xl">
@@ -38,25 +58,41 @@ export default function Home() {
             // my plan for transfer points is for the point to have all colours of routes on transfers */}
             {routes.map(route => (
               <>
+                {/* 
+                    SOrt by stop num to get accurate lines manually go to the stop.js and do this also have to add missing stops
+                    and to close the stop lines repeat the first stop twice but different stopnums as 1 and nth(last)
+                */}
+                <Polyline 
+                  positions={route.stops.sort((a, b) => a.stopNum - b.stopNum).map(stop => stop.coords)}
+                  color={route["color"]}
+                />
                 {route["stops"].map(stop => (
                     <CircleMarker 
                       key={stop.id}
                       center={stop.coords}
                       radius={2}
-                      fillColor={route["color"]}
+                      fillColor="white"
                       color='grey'
                       fillOpacity={4}
                       weight={1}
                     />
                   ))}
-                  {/* 
-                    SOrt by stop num to get accurate lines manually go to the stop.js and do this also have to add missing stops
-                    and to close the stop lines repeat the first stop twice but different stopnums as 1 and nth(last)
-                  */}
-                  <Polyline 
-                    positions={route.stops.sort((a, b) => a.stopNum - b.stopNum).map(stop => stop.coords)}
-                    color={route["color"]}
-                  />
+                  {
+                    busPositions.map( bus => (
+                      <CircleMarker
+                        key={bus.attributes.objectid}
+                        center={[bus.geometry.y, bus.geometry.x]}
+                        radius={5}
+                        fillColor={getBusColor(bus.attributes.created_user)}
+                        color='white'
+                        fillOpacity={4}
+                        weight={1}
+                      />
+                    )
+
+                    )
+                  }
+                  
               </>
 
               
